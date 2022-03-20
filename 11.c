caf9e56d395194afb4c447a93793b27ab0436cc4
crazycheker <54468989+crazycheker@users.noreply.github.com>
2022-03-20 23:31:40 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+printf("ayush\n");
+int num1 = 80, num2 = 40, z;
+z = (num1 > num2);
+z ? printf("First number is greater than second") : printf("Second number is greater than First");
+return 0;
+}
+
---
+++ 9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int length, breadth;
+printf("ayush\n");
+printf("enter the length of rectangle\n"); 
+scanf("%d", &length); 
+printf ("enter breadth of rectangle\n"); 
+scanf("%d", &breadth); 
+printf("area of rectangle is %d", length*breadth);
+return 0;
+}
